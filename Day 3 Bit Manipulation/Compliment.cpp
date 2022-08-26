cd9da1bed4e9e8bd4ba03857144211428fc00ec9
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-08-26 10:59:57 +0530

    Update Compliment.cpp

--- Day 3 Bit Manipulation/Compliment.cpp
+++ Day 3 Bit Manipulation/Compliment.cpp
@@ -1,1 +1,25 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int twosCompliment(int n)
+{
+  int flag = 0, mask = 1;
+  for(int i=0; n>>i != 0; i++)
+  {
+    if(flag == 0 && ((n&mask) != 0))
+    {
+      flag = 1;
+    }
+    else if(flag == 1)
+    {
+      n ^= mask;
+    }
+    mask <<= 1;
+  }
+  return n;
+}
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<twosCompliment(n);
+}
