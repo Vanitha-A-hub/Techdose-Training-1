9fe7315e4291cb5c4ddce9ee4653605299ab9172
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-08-26 10:32:14 +0530

    Update countSetBits.cpp

--- Day 3 Bit Manipulation/countSetBits.cpp
+++ Day 3 Bit Manipulation/countSetBits.cpp
@@ -1,1 +1,19 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int countSetBits(int n)
+{
+  int mask = 1, count = 0;
+  while(mask <= n)
+  {
+    if((n&mask) != 0)
+      count += 1;
+    mask <<= 1;
+  }
+  return count;
+}
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<countSetBits(n);
+}
