ed41b722a3e56f51cb28aa1f45fdd1d9ad47ad83
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-08-26 10:40:38 +0530

    Update checkithBit.cpp

--- Day 3 Bit Manipulation/checkithBit.cpp
+++ Day 3 Bit Manipulation/checkithBit.cpp
@@ -1,1 +1,12 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int checkithBit(int n, int i)
+{
+  return (n&(1<<(i-1))) != 0;
+}
+int main()
+{
+  int n, i;
+  cin>>n>>i;
+  checkithBit(n, i) == 1 ? cout<<"Yes" : cout<<"No";
+}
