114eafbd93fec5f14ca24366a9da0a14e296e17b
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-08-26 10:22:47 +0530

    Update isPowerOf2.cpp

--- Day 3 Bit Manipulation/isPowerOf2.cpp
+++ Day 3 Bit Manipulation/isPowerOf2.cpp
@@ -1,1 +1,11 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int isPowOf2(int n)
+{
+	return (n&(n-1)) == 0;
+}
+int main() {
+	int n;
+	cin>>n;
+	isPowOf2(n) == 1 ? cout<<"Yes" : cout<<"No";
+}
