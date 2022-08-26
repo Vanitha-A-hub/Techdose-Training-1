70d5716cf8c2b2f9ede84883b93771a1998a52e1
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-08-26 15:08:37 +0530

    Update subset.cpp

--- Day 3 Bit Manipulation/Homeworks/subset.cpp
+++ Day 3 Bit Manipulation/Homeworks/subset.cpp
@@ -1,2 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector <vector<char>> generate(vector <char> s, int n)
+{
+  vector <vector<char>> subsets;
+  for(int i=0; i<(1<<n); i++)
+  {
+    vector<char> partial;
+    for(int j=0; j<n; j++)
+    {
+      if((i & (1<<j)) != 0)
+      {
+        partial.push_back(s[j]);
+      }
+    }
+    subsets.push_back(partial);
+  }
+  return subsets;
+}
+int main()
+{
+  int n;
+  cin>>n;
+  vector <char> s;
+  for(int i=0; i<n; i++)
+  {
+    char ch;
+    cin>>ch;
+    s.push_back(ch);
+  }
+  vector <vector<char>> subsets = generate(s, n);
+  for(vector <char> ch: subsets)
+  {
+      for(char i: ch)
+      {
+          cout<<i;
+      }
+      cout<<"\n";
+  }
+}
