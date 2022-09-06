c2f429948881ec67989b2d83d2c6cfa9e21f1154
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-09-06 19:04:58 +0530

    Update nCr.cpp

--- Day-4 Number Theory/nCr.cpp
+++ Day-4 Number Theory/nCr.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector <vector<int>> ncr(int n, int r)
+int ncr(int n, int r)
 {
   vector<vector<int>> dp(n+1);
   for(int i=0; i<=n; i++)
@@ -9,18 +9,19 @@ vector <vector<int>> ncr(int n, int r)
     {
       if(j==0 || j==i)
       {
-        dp[i][j].push_back(1);
+        dp[i].push_back(1);
       }
       else
       {
-        dp[i][j].push_back(dp[i-1][j]+dp[i-1][j-1]);
+        dp[i].push_back(dp[i-1][j]+dp[i-1][j-1]);
       }
     }
   }
+  return dp[n][r];
 }
 int main()
 {
   int n, r;
   cin>>n>>r;
-  vector<vector<int>> dp = ncr(n, r);
+  cout<<ncr(n, r);
 }
