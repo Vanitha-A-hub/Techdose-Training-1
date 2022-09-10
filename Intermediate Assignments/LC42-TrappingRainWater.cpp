0d2d4bc5086808dffa8d55003965cfa57901c55d
Sasireka S <80803086+Sasireka3019@users.noreply.github.com>
2022-09-10 16:31:30 +0530

    Update LC42-TrappingRainWater.cpp

--- Intermediate Assignments/LC42-TrappingRainWater.cpp
+++ Intermediate Assignments/LC42-TrappingRainWater.cpp
@@ -1,1 +1,35 @@
-
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left[n], right[n];
+        int lmax = height[0], rmax = height[n-1];
+        for(int i=0; i<n; i++)
+        {
+            if(height[i] > lmax)
+            {
+                lmax = height[i];
+            }
+            left[i] = lmax;
+        }
+        for(int i=n-1; i>=0; i--)
+        {
+            if(height[i] > rmax)
+            {
+                rmax = height[i];
+            }
+            right[i] = rmax;
+        }
+        int sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            int low;
+            if(left[i]<right[i])
+                low = left[i];
+            else
+                low = right[i];
+            sum += (low-height[i]);
+        }
+        return sum;
+    }
+};
